ebef3f7437d2a6ab934a095311d3026bd86f7ffa
Tim Gilevich <ouncetim@gmail.com>
2024-09-10 02:41:12 +0300

    WIP

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-cmake -DPATCH_VERSION=9999 -DBUILD_TESTS=ON -DCONFIGURE_DEB_PACKAGE=ON --preset linux-release
+cmake -DPROJECT_VERSION="0.0.2" -DBUILD_TESTS=ON -DCONFIGURE_DEB_PACKAGE=ON --preset linux-release
 cmake --build --preset build-linux-release
-cpack -G DEB --preset Default
+cpack -G DEB --preset deb-package-release

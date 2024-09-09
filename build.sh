fa33bdb2e94846cd380d5945558d687f7d977da1
Tim Gilevich <ouncetim@gmail.com>
2024-09-10 01:46:32 +0300

    WIP

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-cmake -DPATCH_VERSION=9999 -DBUILD_TEST=ON -DSETUP_DEB_PACKAGE=ON --preset linux-debug
-cmake --build --preset linux-build-release
+cmake -DPATCH_VERSION=9999 -DBUILD_TEST=ON -DSETUP_DEB_PACKAGE=ON --preset linux-release
+cmake --build --preset build-linux-release

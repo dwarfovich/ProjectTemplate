27f1cfbbf1bfb32a48574d1a8a63faf94446535b
Tim Gilevich <ouncetim@gmail.com>
2024-09-10 01:24:10 +0300

    Init

---
+++ include/header.h
@@ -0,0 +1,5 @@
+#pragma once
+
+int func(){
+    return 55;
+}
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+﻿#include "header.h"
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, world!\n";
+    std::cout << "func() from header.h: " << func() << '\n';
+
+    return 0;
+}
